037e738e5956ee975fb9ab91ce33212e5a580c0d
Chris Bracken <chris@bracken.jp>
2018-11-16 09:58:41 -0800

    Use dmenu to check if user wants to exit
    i3-nagbar requires the mouse. dmenu is more convenient.

---
+++ .i3/exit_check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Use dmenu to check if the user wants to exit i3.
+
+while [ "$choice" != "no" -a "$choice" != "yes" ]; do
+  choice=$(echo -e 'no\nyes' | dmenu -p "Really exit?")
+done
+if [ "$choice" == "yes" ]; then
+  i3-msg exit
+fi
