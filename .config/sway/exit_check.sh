520df5a94eeeca0a127506f6a7ca8a7d90472aba
Chris Bracken <chris@bracken.jp>
2020-03-09 10:00:48 -0700

    Sway: Use slightly larger fonts, support Japanese
    Set slightly larger fonts for Sway window titles, swaybar, and dmenu.
    Enable Japanese support in titles.

--- .config/sway/exit_check.sh
+++ .config/sway/exit_check.sh
@@ -3,7 +3,7 @@
 # Use dmenu to check if the user wants to exit i3.
 
 while [ "$choice" != "no" ] && [ "$choice" != "yes" ]; do
-  choice=$(/bin/echo -e 'no\nyes' | dmenu -p "Really exit?")
+  choice=$(/bin/echo -e 'no\nyes' | dmenu -fn "DejaVu Sans Mono-16" -p "Really exit?")
 done
 if [ "$choice" = "yes" ]; then
   swaymsg exit
