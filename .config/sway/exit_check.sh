675173e51e4147005227b7e09bfa8f1baf70fd1a
Chris Bracken <chris@bracken.jp>
2021-09-08 16:33:26 -0700

    sway: use wofi instead of dmenu
    wofi is a Wayland-compatible re-implementation of rofi.
    https://hg.sr.ht/~scoopta/wofi

--- .config/sway/exit_check.sh
+++ .config/sway/exit_check.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-# Use dmenu to check if the user wants to exit sway.
+# Use wofi to check if the user wants to exit sway.
 
-# If dmenu isn't installed, bail out immediately.
-if [ ! -e /usr/local/bin/dmenu ]; then
+# If wofi isn't installed, bail out immediately.
+which wofi
+if [ $? != 0 ]; then
   swaymsg exit
 fi
 
-# Work around dmenu crasher when XOpenIM() returns NULL.
-# See: https://bugs.archlinux.org/task/61673
-unset XMODIFIERS
-
 while [ "$choice" != "no" ] && [ "$choice" != "yes" ]; do
-  choice=$(echo -e 'no\nyes' | dmenu -fn "DejaVu Sans Mono-16" -p "Really exit?")
+  choice=$(echo -e 'no\nyes' | wofi --show dmenu -p "Really exit?")
 done
 if [ "$choice" = "yes" ]; then
   swaymsg exit
