2b480c2a1b48b900dfede5fe83225c515514f217
Chris Bracken <chris@bracken.jp>
2024-08-23 19:02:17 -0700

    vim: Add rust-analyzer lsp plugin

--- .config/nvim/lsp.lua
+++ .config/nvim/lsp.lua
@@ -1,3 +1,4 @@
 require'lspconfig'.clangd.setup{}
 require'lspconfig'.dartls.setup{}
+require'lspconfig'.rust_analyzer.setup{}
 vim.diagnostic.config{virtual_text = false, signs = false, underline = false, update_in_insert = false};
