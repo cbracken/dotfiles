34de6be640dc9ca9a625cc57338aac8f8b4e129b
Chris Bracken <chris@bracken.jp>
2024-08-25 14:19:31 -0700

    vim: reorder lua imports
    This should matter less now, but ordering of configuration can be
    important. For example leader key configuration needs to happen prior to
    keymapping configuration.
    
    This places options first, then keymappings, then the rest is imported.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 -- Core configuration options.
-require('config.colorscheme')
-require('config.keymappings')
 require('config.options')
+require('config.keymappings')
+require('config.colorscheme')
 require('config.snippets')
 
 -- Package manager.
