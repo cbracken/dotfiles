3e5d22d7bbd9e8383aaeb9c0a97a9ef215d83984
Chris Bracken <chris@bracken.jp>
2024-08-31 08:37:00 -0700

    vim: use quickfix list in place of Trouble
    Trouble makes diagnostics prettier but the reliance on nerdfont support
    is annoying.

--- .config/nvim/lua/plugins/utilities.lua
+++ .config/nvim/lua/plugins/utilities.lua
@@ -1,17 +1,5 @@
 return {
   {
-    "folke/trouble.nvim",
-    opts = {},
-    cmd = "Trouble",
-    keys = {
-      { "<leader>qd", "<cmd>Trouble diagnostics toggle<cr>",                      desc = "Diagnostics", },
-      { "<leader>qD", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",         desc = "Buffer Diagnostics", },
-      { "<leader>qq", "<cmd>Trouble qflist toggle<cr>",                           desc = "Quickfix List", },
-      { "<leader>ql", "<cmd>Trouble lsp toggle focus=true win.size=0.3<cr>",      desc = "LSP Definitions / references / ...", },
-      { "<leader>qs", "<cmd>Trouble symbols toggle focus=false win.size=0.3<cr>", desc = "Symbols", },
-    },
-  },
-  {
     "ibhagwan/fzf-lua",
     opts = {},
   },
