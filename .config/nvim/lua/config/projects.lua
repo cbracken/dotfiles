279c2879568b2acc48b0100702bc4570c0771435
Chris Bracken <chris@bracken.jp>
2025-04-26 16:00:30 -0700

    nvim: update flutter engine path

--- .config/nvim/lua/config/projects.lua
+++ .config/nvim/lua/config/projects.lua
@@ -1,6 +1,6 @@
 -- Updates the vim path for Flutter engine development.
 local function update_path_for_flutter_engine()
-  local engine_dir = vim.fn.expand('$HOME/Developer/flutter/engine/src')
+  local engine_dir = vim.fn.expand('$HOME/Developer/flutter/flutter/engine/src')
   if vim.fn.getcwd():find(engine_dir, 1, true) == 1 then
     vim.opt.path:append(engine_dir)
   end
